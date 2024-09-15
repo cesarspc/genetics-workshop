30760f0d5d74cff1d1a86236857a7cff2aed1c8b
cesarSPC <149351724+cesarSPC@users.noreply.github.com>
2024-09-15 12:09:49 -0500

    Update distributed strategy

--- genetics-workshop/src/DistributedStrategy.java
+++ genetics-workshop/src/DistributedStrategy.java
@@ -6,42 +6,36 @@ import java.util.concurrent.TimeUnit;
 
 public class DistributedStrategy {
 
-    private List<Thread> threads;
     private int nThreads;
     private long start; // Start for measure the execution time
     private long end;
     private ExecutorService executorService;
     
     public DistributedStrategy(int nThreads){
-
-        this.threads = new ArrayList<>();
         this.nThreads = nThreads;
         this.start = System.nanoTime();
         this.executorService = Executors.newFixedThreadPool(nThreads + 1);
     }
 
     public void bootSaver(ArrayList<String> database, int size){
-        threads.clear();
         int sublistSize = size / this.nThreads;
-        int index = 0; // Hand the current position
+        int sequenceIndex = 0; // Hand the current position
 
         for(int i = 0; i < this.nThreads + 1; i++){
             
-            int finalIndex;
-
+            int currentSize;
             // Avoid out of bounds exception
-            finalIndex = (index + sublistSize) > size ? size : index + sublistSize;
+            currentSize = (sequenceIndex + sublistSize) > size ? size - sequenceIndex : sublistSize;
 
-            List<String> sublist = database.subList(index, finalIndex);
-            index = finalIndex;
+            executorService.submit(new SaverThread("data/database.txt", SequenceAnalysis.generateList(currentSize))); // Hand all threads
 
-            executorService.submit(new SaverThread("data/database.txt", sublist)); // Hand all threads
+            sequenceIndex += currentSize;
         }
         
         // Wait for each thread to finish
         executorService.shutdown();
         try {
-            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+            if (!executorService.awaitTermination(30, TimeUnit.SECONDS)) {
                 executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
@@ -57,6 +51,8 @@ public class DistributedStrategy {
 
         this.finishTask();
     }
+
+   
 
     public void finishTask() {
         end = System.nanoTime();
--- genetics-workshop/src/SequenceAnalysis.java
+++ genetics-workshop/src/SequenceAnalysis.java
@@ -1,15 +1,17 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Vector;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 class SequenceAnalysis{
     
     // Parameters
-    public static final int DATABASE_SIZE = 2000000;
+    public static final int DATABASE_SIZE = 10;
     private static final int SEQUENCE_SIZE = 10;
-    private static final int DISTRIBUTION_RATE = 20; // Number of threads for distribute demanding tasks
+    private static final int DISTRIBUTION_RATE = 10; // Number of threads for distribute demanding tasks
     private static final double[] WEIGHTS = {0.25, 0.25, 0.25, 0.25}; // Must total 1
     private static final char[] NUCLEOTIDES = {'A', 'C', 'G', 'T'};
     private static ArrayList<String> database = new ArrayList<String>();
@@ -21,14 +23,6 @@ class SequenceAnalysis{
     // Method for random generation of database
     public static void generateDatabase(){
         DistributedStrategy dStrategy = new DistributedStrategy(DISTRIBUTION_RATE);
-        for(int i = 0; i < DATABASE_SIZE; i++){
-            String sequence = "";
-            for(int j = 0; j < SEQUENCE_SIZE; j++){
-                sequence += pickNucleotide();
-            }
-            database.add(sequence);
-        }
-
         // Save txt
         dStrategy.bootSaver(database, DATABASE_SIZE);
     }
@@ -46,5 +40,21 @@ class SequenceAnalysis{
         }
 
         return ' ';
+    }
+
+    public static String generateSequence(){
+        String sequence = "";
+            for(int i = 0; i < SEQUENCE_SIZE; i++){
+                sequence += pickNucleotide();
+            }
+        return sequence;
+    }
+
+    public static List<String> generateList(int n){
+        List<String> sublist = new Vector<>();
+            for(int i = 0; i < n; i++){
+                sublist.add(generateSequence());
+            }
+        return sublist;
     }
 }
